087372479fd73c652ec7b8150a780f438e461715
marcelinus <marcelinusilonuba21@gmail.com>
2025-02-22 03:05:57 +0100

    fix: add corl to main

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from datetime import datetime, timedelta
 from telex_settings import integration_bp
 from apscheduler.schedulers.background import BackgroundScheduler
 from fetch_stock_price import fetch_mtn_stock_price
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(integration_bp)
 
